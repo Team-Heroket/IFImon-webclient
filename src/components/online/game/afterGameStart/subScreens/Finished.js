115b30fc03c0da27ba31ebc6ab85505d06d48ec3
Alexander Theus <alexander.theudahl@gmail.com>
2020-04-24 12:02:39 +0200

    Updated Finished state: avatarID now has no error

--- src/components/online/game/afterGameStart/subScreens/Finished.js
+++ src/components/online/game/afterGameStart/subScreens/Finished.js
@@ -19,16 +19,15 @@ const Label = styled.label`
   position: relative;
   transform : translate(-50%, 0%);
   width: 400px;
-  left: 15%;
+ 
   color: white;
-  margin-left: 4px;
+  
   margin-bottom: 50px;
   text-transform: uppercase;
   font-size: 30px;
   font-weight: 300;
 `;
 
-let linkToImage = '../../../../../components/shared/images/avatarSVG';
 
 
 export let Finished = ({masterState, history}) => {
@@ -49,8 +48,8 @@ export let Finished = ({masterState, history}) => {
         <div>
             <FormContainer>
                 <Space/>
-                {masterState.winners[0].user.avatarId < 10 ? <img alt="avatar" src={require('../../../../../components/shared/images/avatarSVG/00'+(masterState.winners[0].user.avatarId)+'-avatar.svg')} height={"150px"} width={"150px"}/>
-                    : <img alt="avatar" src={require(linkToImage+'/0'+(masterState.winners[0].user.avatarId)+'-avatar.svg')} height={"66px"} width={"66px"}/>}
+                {masterState.winners[0].user.avatarId.valueOf() < 10 ? <img alt="avatar" src={require('../../../../../components/shared/images/avatarSVG/00'+(masterState.winners[0].user.avatarId)+'-avatar.svg')} height={"150px"} width={"150px"}/>
+                    : <img alt="avatar" src={require('../../../../../components/shared/images/avatarSVG/0'+(masterState.winners[0].user.avatarId)+'-avatar.svg')} height={"150px"} width={"150px"}/>}
                 <div>
                     <Space/>
                     <Label>
@@ -59,7 +58,6 @@ export let Finished = ({masterState, history}) => {
                     <Space/>
                     <LogOutButton
                         width = "50%"
-                        disabled={masterState.amITurnPlayer}
                         onClick={() => { if (window.confirm('Are you sure you want to leave the game?')) history.push('/menu') }} > Leave
                     </LogOutButton>
                 </div>
